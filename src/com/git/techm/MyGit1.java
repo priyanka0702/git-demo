dca7ece6bc33dd061f6be92704b8e88ead585462
priyanka <priyankaa724@gmail.com>
2019-03-27 23:00:54 +0530

    Modified new logic

--- src/com/git/techm/MyGit1.java
+++ src/com/git/techm/MyGit1.java
@@ -4,5 +4,8 @@ public class MyGit1 {
 	public static void main(String[] args) {
 		System.out.println("My new base class");
 		System.out.println("Addition");
+		System.out.println("Sub");
+		
+				
 	}
 }
